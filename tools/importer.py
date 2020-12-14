67794d35604634f552d3821061a8a78260680737
mc-cat-tty <>
2020-12-14 09:30:48 +0100

    Parser works

--- tools/importer.py
+++ tools/importer.py
@@ -23,13 +23,15 @@ class DataType(Enum):
     SPECIES = 3
     EXAMPLE = 4
 
+
 class Parser:
-    def __init__(self, line_structure: str = r"^\"(\w+?) ([\s\w\d,]+)\"[ \w]*$") -> None:
+    def __init__(self, line_structure: str = r"^\"(\w+?) ([\s\w\d,'\(\)\.&\-]+)\"[ \w]*$") -> None:
         self.line_structure: str = line_structure
         self.current_class: str = ""
         self.current_family: str = ""
         self.current_species: str = ""
         self.current_family_code = "AA"
+        self.gen = self.next_family_code()
 
     def next_family_code(self) -> str:
         """
@@ -39,7 +41,7 @@ class Parser:
         for first_char in string.ascii_uppercase:
             for second_char in string.ascii_uppercase:
                 self.current_family_code = first_char+second_char
-                yield first_char+second_char
+                yield first_char + second_char
 
     def parse_line(self, line: str) -> Tuple[DataType, List[str]]:
         """
@@ -47,7 +49,8 @@ class Parser:
         :param line: line to be parsed
         :return: parsed line; namely a list of all the different elements, according to the type of line (class, family, species or example)
         """
-        if not line:
+        logging.info(f"Parsing {line}")
+        if not line or line == "\n":
             return DataType.BLANK_LINE, []
         parsed_line: Sequence[str] = re.match(self.line_structure, line).groups()
         if parsed_line[0] == "Classe":
@@ -55,10 +58,10 @@ class Parser:
             return DataType.CLASS, [self.current_class]
         elif parsed_line[0] == "Famiglia":
             self.current_family = parsed_line[1]
-            self.current_family_code = next(self.next_family_code())
+            self.current_family_code = next(self.gen)
             return DataType.FAMILY, [self.current_family, self.current_family_code]
         else:
-            parsed_example = re.match(r"^([ a-z]+)[ \(]*([\w \.&']+), ?(\d+)[\) ]*", parsed_line[1].strip()).groups()
+            parsed_example: Sequence[str] = re.match(r"^([ a-z\.]+)[ \(]*([\w \.&'\-,]+), ?(\d+)[\) ]*", parsed_line[1].strip()).groups()
             return DataType.SPECIES, [parsed_line[0]+parsed_example[0],  # Name
                                       parsed_example[1],  # Discoverer
                                       parsed_example[2]]  # Year
@@ -66,14 +69,28 @@ class Parser:
 
 
 def main(filename: str, host: str, port: int, username: str, password: str) -> None:
-    pass
+    parser: Parser = Parser()
+    with open(filename, "r") as file:
+        lines: List[str] = file.readlines()
+    for line in lines:
+        try:
+            data_type, data = parser.parse_line(line)
+        except:
+            logging.error(f"{line} NOT IMPORTED")
+        else:
+            if data_type == DataType.BLANK_LINE:
+                logging.info(f"{data_type} discarded")
+            else:
+                logging.info(f"{data_type} {data} imported")
+
+
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s: %(message)s")
+    logging.basicConfig(level=logging.WARNING, format="%(asctime)s - %(levelname)s: %(message)s")
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--filename", help="Filename path", default=FILENAME, type=str, dest="filename")
-    parser.add_argument("-h", "--host", help="MariaDB (MySQL) server address", default=HOST, type=str, dest="host")
+    parser.add_argument("-a", "--address", help="MariaDB (MySQL) server address", default=HOST, type=str, dest="host")
     parser.add_argument("-P", "--port", help="MariaDB (MySQL) server port number", default=PORT, type=int, dest="port")  # Notice that this flag is uppercase
     parser.add_argument("-u", "--username", help="MariaDB (MySQL) username", default=USERNAME, type=str, dest="username")
     parser.add_argument("-p", "--password", help="MariaDB (MySQL) password", default=PASSWORD, type=str, dest="password")
