c88facaca07fbd0ab1227753e678b65374742364
mc-cat-tty <>
2020-12-18 18:07:57 +0100

    importer.py can act like an adder with --add

--- tools/importer.py
+++ tools/importer.py
@@ -27,12 +27,13 @@ class DataType(Enum):
 
 
 class Parser:
-    def __init__(self, line_structure: str = r"^\"(\w+?) ([\s\w\d,'\(\)\.&\-]+)\"[ \w]*$") -> None:
+    def __init__(self, starting_family_code: str = "AA", line_structure: str = r"^\"(\w+?) ([\s\w\d,'\(\)\.&\-]+)\"[ \w]*$") -> None:
         self.line_structure: str = line_structure
         self.current_class: str = ""
         self.current_family: str = ""
         self.current_species: str = ""
-        self.current_family_code = "AA"
+        self.starting_family_code: str = starting_family_code
+        self.current_family_code: str = starting_family_code
         self.gen = self.next_family_code()
 
     def next_family_code(self) -> str:
@@ -40,8 +41,8 @@ class Parser:
         This method provides a generator for the unique family code. I starts from AA identifier
         :return: next family code
         """
-        for first_char in string.ascii_uppercase:
-            for second_char in string.ascii_uppercase:
+        for first_char in string.ascii_uppercase[string.ascii_uppercase.find(self.starting_family_code[0]):]:
+            for second_char in string.ascii_uppercase[string.ascii_uppercase.find(self.starting_family_code[1]):]:
                 self.current_family_code = first_char + second_char
                 yield first_char + second_char
 
@@ -139,6 +140,34 @@ class DBConnector:
                          [species_name, discoverer, year, family_id])
         self.last_inserted_species_id = self.get_last_inserted_id()
 
+    def add_class(self, class_name: str) -> None:
+        try:
+            self.insert_class(class_name)
+        except mariadb.IntegrityError:  # If already in the database
+            self.cur.execute('SELECT id FROM genere WHERE nome = ?', [class_name])
+            self.last_inserted_class_id = int(self.cur.fetchone[0])
+
+    def add_family(self, family_name: str, family_code: str, class_id: int = None) -> None:
+        if self.last_inserted_class_id is None:
+            self.cur.execute('SELECT genere_id FROM famiglia WHERE nome = ?', [family_name])
+            self.last_inserted_class_id = int(self.cur.fetchone()[0])
+        try:
+            self.insert_family(family_name, family_code, class_id)
+        except mariadb.IntegrityError:  # If already in the database
+            self.cur.execute('SELECT id FROM famiglia WHERE nome = ?', [family_name])
+            self.last_inserted_family_id = int(self.cur.fetchone()[0])
+
+    def add_species(self, species_name: str, discoverer: str, year: int, family_id: int = None) -> None:
+        try:
+            self.insert_species(species_name, discoverer, year, family_id)
+        except mariadb.IntegrityError:  # If already in the database
+            self.cur.execute('SELECT id FROM specie WHERE nome = ?', [species_name])
+            self.last_inserted_species_id = int(self.cur.fetchone()[0])
+
+    def get_last_family_code(self) -> str:
+        self.cur.execute('SELECT max(codice) FROM famiglia')
+        return self.cur.fetchone()[0]
+
     def get_last_inserted_id(self) -> int:
         self.cur.execute('SELECT @@IDENTITY')
         last_id = self.cur.fetchone()[0]
@@ -146,20 +175,29 @@ class DBConnector:
         return last_id
 
 
-def main(filename: str, host: str, port: int, username: str, password: str, database: str) -> None:
+def main(filename: str, host: str, port: int, username: str, password: str, database: str, add: bool) -> None:
     db_connector: DBConnector = DBConnector(host, port, username, password, database)
-    line_parser: Parser = Parser()
+    line_parser: Parser = Parser(db_connector.get_last_family_code())
     with open(filename, "r") as file:
         lines: List[str] = file.readlines()
     for line in lines:
         try:
             data_type, data = line_parser.parse_line(line)
-            if data_type is DataType.CLASS:
-                db_connector.insert_class(data[0])
-            elif data_type is DataType.FAMILY:
-                db_connector.insert_family(data[0], data[1])
-            elif data_type is DataType.SPECIES:
-                db_connector.insert_species(data[0], data[1], int(data[2]))
+            if not add:  # Standard import
+                if data_type is DataType.CLASS:
+                    db_connector.insert_class(data[0])
+                elif data_type is DataType.FAMILY:
+                    db_connector.insert_family(data[0], data[1])
+                elif data_type is DataType.SPECIES:
+                    db_connector.insert_species(data[0], data[1], int(data[2]))
+            else:  # Add records
+                if add:
+                    if data_type is DataType.CLASS:
+                        db_connector.add_class(data[0])
+                    elif data_type is DataType.FAMILY:
+                        db_connector.add_family(data[0], data[1])
+                    elif data_type is DataType.SPECIES:
+                        db_connector.add_species(data[0], data[1], int(data[2]))
         except:
             logging.error(f"{line} NOT IMPORTED")
         else:
@@ -183,7 +221,7 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--database", help="MariaDB (MySQL) database to be used", default=DATABASE, type=str,
                         dest="database")
     parser.add_argument("--add", help="Add records to the database", default=False,
-                        action=argparse.BooleanOptionalAction)  # Enable this flag to append data to existing
+                        action="store_true", dest="add")  # Enable this flag to append data to existing
     # families. If the family doesn't exist, the script will create it automatically
     args = parser.parse_args()
-    main(args.filename, args.host, args.port, args.username, args.password, args.database)
+    main(args.filename, args.host, args.port, args.username, args.password, args.database, args.add)
